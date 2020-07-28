5add766d254417ce7f3c80a8bc6c4c788ec1e1af
Zhiming Wang <i@zhimingwang.org>
2020-07-28 23:05:13 +0800

    Implement gallery page

--- cards.js
+++ cards.js
@@ -18,6 +18,7 @@ const githubCSSPath = path.join(
   templatesDir,
   "github-536f87adf120294bb4c70c32cb21f032.css"
 );
+const galleryHTMLPath = path.join(templatesDir, "gallery.html");
 
 // Generate images into docs/ for GitHub Pages.
 const generatedDir = path.join(__dirname, "docs");
@@ -87,6 +88,7 @@ const formatCount = count => {
   const cardHTML = await fs.readFile(cardHTMLPath, "utf8");
   const frameworksCSS = await fs.readFile(frameworksCSSPath, "utf8");
   const githubCSS = await fs.readFile(githubCSSPath, "utf8");
+  const galleryHTML = await fs.readFile(galleryHTMLPath, "utf8");
 
   const browser = await puppeteer.launch({
     // I know! Use root with discretion.
@@ -155,4 +157,14 @@ const formatCount = count => {
     console.error(`error: ${err.message}`);
     process.exitCode = 1;
   }
+
+  const galleryHTMLRendered = Mustache.render(galleryHTML, {
+    images: screenshotPaths.map(screenshotPath => ({
+      url: path.relative(generatedDir, screenshotPath),
+    })),
+  });
+  await fs.writeFile(
+    path.join(generatedDir, "gallery.html"),
+    galleryHTMLRendered
+  );
 })();
